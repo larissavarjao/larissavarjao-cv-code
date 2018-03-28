f54424a352c57330f0b205636ffe5783b2fcca96
Larissa Varjão <larissavarjao@Macbook-Pro-Larissa.local>
2018-03-28 12:47:25 -0300

    new structure and new cover of website

--- src/containers/Cover/index.tsx
+++ src/containers/Cover/index.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 
 import Header from '../../components/Header';
-import Abstract from '../../components/Abstract';
 
 const s = require('./style.scss');
 
 export default class Cover extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className={s.container}>
+      <div className={s.cover}>
         <Header />
-        <Abstract />
+        <div className={s.background} />
+        <div className={s.coverImg}>
+          <h1>Larissa Varjão</h1>
+          <h2>Front-End Developer</h2>
+          <div className={s.buttons}>
+            <div className={`${s.button} ${s.buttonGreen}`}>Business</div>
+            <div className={`${s.button} ${s.buttonBlack}`}>Developer</div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,46 +1,17 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Fullpage, Slide, HorizontalSlider } from 'fullpage-react';
 import Cover from '../Cover';
 
 const s = require('./style.scss');
 
 interface IProps {}
 
-const fullPageOptions = {
-  scrollSensitivity: 7,
-  touchSensitivity: 7,
-  scrollSpeed: 500,
-  hideScrollBars: true,
-  enableArrowKeys: true
-};
-
-const horizontalSliderProps = {
-  name: 'horizontalSlider1', // name is required
-  infinite: true // enable infinite scrolling
-};
-
-const horizontalSlides = [
-  <Slide> Slide 2.1 </Slide>,
-  <Slide> Slide 2.2 </Slide>
-];
-horizontalSliderProps.slides = horizontalSlides;
-
-const slides = [
-  <Slide>
-    <Cover />
-  </Slide>,
-  <HorizontalSlider {...horizontalSliderProps} />,
-  <Slide> Slide 3 </Slide>
-];
-fullPageOptions.slides = slides;
-
 @observer
 export default class Home extends React.Component<IProps, {}> {
   public render() {
     return (
       <div className={s.container}>
-        <Fullpage {...fullPageOptions} />
+        <Cover />
       </div>
     );
   }
